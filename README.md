cc8996d105c9b12cdee2a3b9ddd86ad0f2df2b66
Frank Klingebiel <klingebiel@optimal-systems.de>
2018-01-10 17:23:07 +0100

    Added excel example and moved readme contents to this project.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 Eureka integration with express.js
 ----------------------------------
+
+See example folder for usage.
+
+
+Environment variables
+---------------------
+
+The following environment variables are used.
+
+### EUREKA_HOST
+Default: `localhost`
+
+This variable determines the eureka server to be used. This may be a ip address or hostname. This must be set, if the eureka service is running on a different machine.
+
+### EUREKA_PORT
+Default: `7261`
+
+The eureka registry port.
+
+### EUREKA_SERVICEPATH
+Default: `/eureka/apps`
+
+The eureka service path prefix. It should seldom be needed to change this.
+
+### APP_PORT
+Default: `7461`
+
+The public port of the application.
+
+### APP_PROFILE
+Default: ``
+
+The profiles of the application. This determines which configuration is in use.
+
+### APP_NAME
+Default: ``
+
+The name (appId) of the application as registered with the eureka registry.
+
+### PUBLIC_HOST
+Default: `os.hostname()`
+
+This is the hostname that is used during registration with the eureka registry. It may be needed to set this variable, if the service is running inside a docker container. This variable determines how the eureka service can reach the service.
+
+###  PUBLIC_PORT
+Default: Same as `APP_PORT`.
+
+This tells the eureka registery under which port the service is reachable. It may be needed to set this variable, if the service is running inside a docker container or behind a proxy service.
---
+++ examples/excel.js
@@ -0,0 +1,39 @@
+/**
+ * Example (micro) service that uses https://www.npmjs.com/package/xlsx to extract data from excel sheets.
+ * This is for demonstation usage only. For production the propper error handling is missing.
+ * You must 'npm install xlsx' before you can run this excample by running 'node example/excel.js'.
+ *
+ * If the service is registered withing the registry, every one can call 'POST /ExcelReader' with a excel
+ * file as body to retrieve a JSON with all data inside the excel worksheet.
+ */
+const eureka = require('../eureka.js');
+const express = require('express')
+const bodyParser = require('body-parser')
+const xlsx = require('xlsx')
+
+// Create a express app
+const app = express()
+
+// All requests are handled raw, we use the body-parser raw middleware for this.
+app.use(bodyParser.raw({
+  type : '*/*'
+}))
+
+// The one and only endpoint method. This method accepts a raw excel file and tries to parse it.
+app.post('/', (req, res) => {
+  // The request body is a Buffer object. We are using xlsx now to parse it.
+  var workbook = xlsx.read( req.body, {type:'buffer'} )
+  // If ok, just send the result as json.
+  res.json( workbook )
+})
+
+// Start up the express application ...
+var listener = app.listen(3000, () => {
+  // ... and register with the eureka discovery
+  // Hint: If your eureka registry is not on localhost, you must provide a EUREKA_HOST environment variable.
+  eureka.init({
+      name : 'ExcelReader',                 // We need a name for the service, so others can use it.
+      express : app,                        // The express app is used to provide the discovery endpoints
+      port : listener.address().port        // This is the public port for the registry, we just pass the express listener port
+  })
+})
